45b634c5e3b89e313323d62f78f17b547c9308f1
Alvin Valdez <32502043+AlvinValdez@users.noreply.github.com>
2024-07-04 14:20:39 -0400

    Cleaned up file and removed website logo being added

--- main.py
+++ main.py
@@ -5,10 +5,8 @@
 import os
 import sys
 import zipfile
-from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import requests
-from os.path  import basename
 def get_chapters(url):
     test_site = url
     hdr = {
@@ -20,17 +18,16 @@ def get_chapters(url):
         'Connection': 'keep-alive'}
     site = requests.get(test_site)
     soup = BeautifulSoup(site.text)
+    #print(soup)
     ahref = soup.find("div", {"class": "group-box list",}).find_all('a', href=True)
     links= []
     for a in ahref:
         links.append(a['href'])
 
+    print(links)
     scrape_chapters(links)
 
 def scrape_chapters(chapters):
-    #print(chapters)
-    #chaptername = chapters[0]
-    #print(chaptername.split('/')[3])
     hdr = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.102 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -39,7 +36,7 @@ def scrape_chapters(chapters):
         'Accept-Language': 'en-US,en;q=0.9',
         'Connection': 'keep-alive'}
     for count, value in enumerate(chapters):
-        print(count)
+        #print(count)
 
         site = requests.get(chapters[count])
         soup = BeautifulSoup(site.text)
@@ -49,14 +46,18 @@ def scrape_chapters(chapters):
         pictures= soup.find_all('img', src=True)
         picturelinks=[]
         for a in pictures:
-            picturelinks.append(a['src'])
+            if a['src'] == 'https://readallcomics.com/wp-content/uploads/2020/09/logo-1.png':
+                print('Skipping Website Logo')
+            else:
+                picturelinks.append(a['src'])
 
-        print(picturelinks)
+        #print(picturelinks)
         counter=1
+
         with zipfile.ZipFile(splitschaptername+'.cbr', 'w') as img_zip:
             for image_url in picturelinks:
-
                 img_name = os.path.basename(str(counter).zfill(3)+'.jpg')
+                print('Zipping up file: ', img_name)
                 img_data = requests.get(image_url).content
                 img_zip.writestr(img_name, img_data)
                 counter+=1
