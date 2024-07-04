555302b66712546ebcda2f36566eba6d89da7ada
Alvin Valdez <32502043+AlvinValdez@users.noreply.github.com>
2024-07-03 20:00:01 -0400

    Added .CBR zipping of images and use of args in cmd
    .CBR creation functionality added using zipfile. Files are zipped into CBR in sequential order.
    
    Also added the usage of first cmdline args for getting link that user provides

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
+import sys
+import zipfile
 from urllib.request import Request, urlopen
-
 from bs4 import BeautifulSoup
 import requests
 from os.path  import basename
 def get_chapters(url):
-    test_site = 'https://readallcomics.com/category/batman-last-knight-on-earth/'
+    test_site = url
     hdr = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.102 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -37,17 +38,31 @@ def scrape_chapters(chapters):
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en-US,en;q=0.9',
         'Connection': 'keep-alive'}
-    site = requests.get(chapters[0])
-    soup = BeautifulSoup(site.text)
+    for count, value in enumerate(chapters):
+        print(count)
 
-    pictures= soup.find_all('img', src=True)
-    picturelinks=[]
-    for a in pictures:
-        picturelinks.append(a['src'])
+        site = requests.get(chapters[count])
+        soup = BeautifulSoup(site.text)
 
-    #print(picturelinks)
+        chaptername = chapters[count]
+        splitschaptername=chaptername.split('/')[3]
+        pictures= soup.find_all('img', src=True)
+        picturelinks=[]
+        for a in pictures:
+            picturelinks.append(a['src'])
+
+        print(picturelinks)
+        counter=1
+        with zipfile.ZipFile(splitschaptername+'.cbr', 'w') as img_zip:
+            for image_url in picturelinks:
+
+                img_name = os.path.basename(str(counter).zfill(3)+'.jpg')
+                img_data = requests.get(image_url).content
+                img_zip.writestr(img_name, img_data)
+                counter+=1
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_chapters('hi')
+    get_chapters(sys.argv[1])
 
